caae127f4f7d875f55ec36d069b5168731d98906
Sharandeep Ghosh <sharandeep23@yahoo.com>
2020-06-24 05:09:13 +0600

    REFACTORING

--- index.js
+++ index.js
@@ -1,71 +1,50 @@
-// Audio elements
-
-const crash = new Audio("sounds/crash.mp3");
-const kick = new Audio("sounds/kick-bass.mp3");
-const snare = new Audio("sounds/snare.mp3");
-const tom1 = new Audio("sounds/tom-1.mp3");
-const tom2 = new Audio("sounds/tom-2.mp3");
-const tom3 = new Audio("sounds/tom-3.mp3");
-const tom4 = new Audio("sounds/tom-4.mp3");
-
 // Event listener for clicks on the Drum kits
 document.querySelector(".set").addEventListener("click", function (e) {
     // Listener function works only for the BUTTON tag
     if (e.target.tagName === "BUTTON") {
-        switch (e.target.textContent) {
-            case "w":
-                crash.play();
-                break;
-            case "a":
-                kick.play();
-                break;
-            case "s":
-                snare.play();
-                break;
-            case "d":
-                tom1.play();
-                break;
-            case "j":
-                tom2.play();
-                break;
-            case "k":
-                tom3.play();
-                break;
-            // If none of them mathces then it's obviously "l"
-            default:
-                tom4.play();
-        }
+        makeSound(e.target.textContent);
     }
 });
 
 // Event listener for keyboard Events
+document.addEventListener("keydown", function (e) {
+    makeSound(e.key);
+});
 
-document.body.addEventListener("keydown", function (e) {
-    switch (e.key) {
+// Making sound based on keys/chars
+function makeSound(key) {
+    switch (key) {
         case "w":
+            const crash = new Audio("sounds/crash.mp3");
             crash.play();
             break;
         case "a":
+            const kick = new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
         case "s":
+            const snare = new Audio("sounds/snare.mp3");
             snare.play();
             break;
         case "d":
+            const tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
         case "j":
+            const tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
         case "k":
+            const tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
         case "l":
+            const tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
-        // For keyboards any keys can be pressed
-        // thats why defualt comes into action unlike previous switch statement
+
+        // If it reaches to the default, then it's surely for the keyboard event
         default:
-            alert(`Unknown Key(${e.key.toUpperCase()}) Pressed 💀`);
+            alert(`Unknown Key(${key.toUpperCase()}) Pressed 💀!`);
     }
-});
+}
